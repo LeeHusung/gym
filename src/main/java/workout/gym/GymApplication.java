28c1022b0b8efdfb0fb74a89ccc9b1b9f8cbe36b
LeeHusung <mn040820@naver.com>
2023-07-20 19:40:09 +0900

    first commit

---
+++ src/main/java/workout/gym/GymApplication.java
@@ -0,0 +1,13 @@
+package workout.gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/workout/gym/GymApplicationTests.java
@@ -0,0 +1,13 @@
+package workout.gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
